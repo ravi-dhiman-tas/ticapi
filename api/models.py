2961a9618aa62f12fbfa277f912085da3eb13516
pankul <pankul@technoarchsoftwares.com>
2017-04-09 22:04:01 +0530

    integrate cros header package

--- api/models.py
+++ api/models.py
@@ -25,7 +25,7 @@ class Task(models.Model):
     seq = models.CharField(max_length=255)
     delete = models.BooleanField(default=False)
     name = models.CharField(max_length=255)
-    status = models.IntegerField(max_length=255, choices=STATUS_CHOICES, default=0)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
     description = models.TextField()
     viewed = models.IntegerField(default=0)
     user = models.ForeignKey(User)
--- ticapi/settings.py
+++ ticapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -103,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Cross origin config
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Rest framework config
 REST_FRAMEWORK = {
