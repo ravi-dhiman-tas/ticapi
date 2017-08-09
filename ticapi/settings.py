c177bd63742141862d0c1d8d6c66f891903fe882
ravid7000 <ravi@technoarchsoftwares.com>
2017-08-09 23:45:31 +0530

    Changes updated

--- ticapi/settings.py
+++ ticapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=x0pc*0z5kk1@profdm143f+6+=rmk3ufslir3&+8f@o#&@)$9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
