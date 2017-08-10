6debb4159bce2f310a3538b4e83c0ed1085c15c6
ravid7000 <ravi@technoarchsoftwares.com>
2017-08-10 20:11:29 +0530

    Hotfix react router

--- ticapi/settings.py
+++ ticapi/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '=x0pc*0z5kk1@profdm143f+6+=rmk3ufslir3&+8f@o#&@)$9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
